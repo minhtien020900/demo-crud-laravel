bf03cd8e40c0fc9f3880a37e5b4cc885bc054bd6
minhtien020900 <minhtien020900@gmail.com>
2021-05-06 00:38:37 +0700

    Edit template for view of route('/welcome') ò

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,9 +20,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div class="container">
     <div id="app">
-        
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
@@ -77,13 +75,13 @@
                     </ul>
                 </div>
             </div>
-            
+
         </nav>
-    
+
         <main class="py-4">
             @yield('content')
         </main>
-    </div>
+
 </div>
 </body>
 </html>
